3adaaeabb7bca4aee16546792ea76bfe99929e33
Scilla <priscilla.ruiz.tech@gmail.com>
2021-08-24 09:28:08 -0700

    init

---
+++ src/Components/About.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default function About(){
+    return(
+        <div>
+            <section>
+                Hi, my name is Priscilla Seal.
+            </section>
+            <section>
+                I love working with JavaScript. 
+            </section>
+        </div>
+    );
+}
