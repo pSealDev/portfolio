aff23f2419b43d4c0a134d777c8979d4cb6d2892
pSealDev <priscilla.seal.tech@gmail.com>
2022-02-15 10:00:46 -0800

    added link address to github icon and linkedin icon

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,10 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className="footer">
-            <div id="footer_copy">@priscilla seal {new Date().getFullYear()} 
+            <div id="footer_copy">@priscilla seal {new Date().getFullYear()} , 
             all rights reserved &nbsp;
-            <i class="fab fa-github"></i>&nbsp;&nbsp;
-            <i class="fab fa-linkedin"></i>&nbsp;&nbsp;
-            <i class="fas fa-code"></i>&nbsp;&nbsp;
+           <a href="https://github.com/pSealDev"><i class="fab fa-github"></i></a>&nbsp;&nbsp;
+            <a href="https://www.linkedin.com/in/priscillaseal/"><i class="fab fa-linkedin"></i></a>&nbsp;&nbsp;
             </div>
         </div>
     );
